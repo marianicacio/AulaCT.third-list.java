e9675066ad155f830bd32d67d483adb3ea5be467
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-10-04 16:01:22 -0300

    feat: exercise A

---
+++ ExA_mariaeduarda.java
@@ -0,0 +1,13 @@
+public class ExA_mariaeduarda {
+
+	public static void main(String[] args) {
+		int i, res;
+		
+		for (i=15; i<=200; i++ ) {
+			res = i * i;
+			System.out.println("O resultado é: " + res);
+		}
+
+	}
+
+}
