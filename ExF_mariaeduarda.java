ecbec4890c843637b208c5d5c026e813a943c0e5
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-10-04 16:02:51 -0300

    feat: exercise F

---
+++ ExF_mariaeduarda.java
@@ -0,0 +1,13 @@
+public class ExF_mariaeduarda {
+    public static void main(String[] args) {
+		int i;
+		
+		for(i=0; i<=200; i++) {
+			if(i%4 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
