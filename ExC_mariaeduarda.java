15d3efb92fe7c6a56c2abcb67889c0b850a0a3c3
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-10-04 16:02:18 -0300

    feat: exercise C

---
+++ ExC_mariaeduarda.java
@@ -0,0 +1,12 @@
+public class ExC_mariaeduarda {
+    public static void main(String[] args) {
+		int i, res = 0;
+		
+		for(i=0; i<=100; i++) {
+			res += i;
+		}
+		
+		System.out.println("A soma dos numeros é: " +  res);
+
+	}
+}
